81f9c513bae247661b01ebed0b034a1ddb1fee49
Jacob Ahnstedt <ahnstedt.j@gmail.com>
2023-03-01 13:09:41 +0100

    Added a basic ranged AI minion
    - Added SAICharacter
    - Added SAIController
    - Added SBTService_CheckAttackRange
    - Added SBTTask_RangedAttack
    - Added MinionControllerBP
    - Added MinionRangedBB
    - Added MinionRangedBP
    - Added MinionRangedBT
    - Added Query_FindNearbyLocation
    - Added QueryContext_TargetActor
    - Changed IdleToRun interpolation from 1.5 to 8.0

---
+++ Source/ActionRogueLike/Private/AI/SAICharacter.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SAICharacter.h"
+#include "Perception/PawnSensingComponent.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values
+ASAICharacter::ASAICharacter()
+{
+    PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>("PawnSensingComp");
+}
+
+void ASAICharacter::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+
+    PawnSensingComp->OnSeePawn.AddDynamic(this, &ASAICharacter::OnPawnSeen);
+}
+
+void ASAICharacter::OnPawnSeen(APawn* Pawn)
+{
+    AAIController* AIC = Cast<AAIController>(GetController());
+    if (AIC)
+    {
+        UBlackboardComponent* BBComp = AIC->GetBlackboardComponent();
+        BBComp->SetValueAsObject("TargetActor", Pawn);
+
+        DrawDebugString(GetWorld(), GetActorLocation(), "PLAYER SPOTTED", nullptr, FColor::White, 4.0f, true);
+    }
+}
